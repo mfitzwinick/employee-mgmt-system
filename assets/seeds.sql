b9212f61f165eff62bca06f01af0097434259679
Michele Fitzgerald-Winick <mfitzwinick@gmail.com>
2020-12-29 23:00:08 -0800

    adding inquirer questions

--- assets/seeds.sql
+++ assets/seeds.sql
@@ -2,14 +2,14 @@ INSERT INTO department (id, name)
 VALUES (1,"sales"), (2, "operations"), (3, "finance"), (4,"distribution");
 
 INSERT INTO role (id, name, salary, department_id)
-VALUES (20,"sales",70,000,1),
-(21,"sales_manager", 80,000,1),
-(22,"admin_assistant",30,000,2),
-(23,"ops_lead",45,000,2),
-(24,"CFO",65,000,3),
-(25,"accountant",50,000,3),
-(26,"lift_operator",35,000,4),
-(27,"truck_driver",40,000,4);
+VALUES (20,"sales",70000,1),
+(21,"sales_manager", 80000,1),
+(22,"admin_assistant",30000,2),
+(23,"ops_lead",45000,2),
+(24,"CFO",65000,3),
+(25,"accountant",50000,3),
+(26,"lift_operator",35000,4),
+(27,"truck_driver",40000,4);
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
 VALUES (100, "Harry", "Styles", 20, 75),
