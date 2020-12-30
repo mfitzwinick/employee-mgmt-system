8519b6758473749757938a63fbd2b0b8d21980db
Michele Fitzgerald-Winick <mfitzwinick@gmail.com>
2020-12-29 23:46:20 -0800

    Adding functions

--- index-inquirer.js
+++ index-inquirer.js
@@ -11,10 +11,10 @@ var connection = mysql.createConnection({
 // connect to the mysql server and sql database
 connection.connect(function(err) {
     if (err) throw err;
-    // run the start function after the connection is made to prompt the user
+// run the start function//
     start();
   });
-  // function which prompts the user for what action they should take
+// function which prompts the user for what action they should take//
 function start() {
     inquirer
     .prompt({
@@ -34,10 +34,11 @@ function start() {
     else if (answer.drm === "EMPLOYEE") {
     accessEmployee();
   }
-    else (answer.drm === "EXIT") {
+    else if (answer.drm === "EXIT") {
     connection.end();
   }
 });
+}
 
 function accessEmployee() {
     inquirer
@@ -75,7 +76,7 @@ function accessEmployee() {
       })
       .then(function(answer) {
         if (answer.department === "ADD") {
-          addDepartment();
+            addDepartment();
         }
         else if(answer.department === "UPDATE") {
             updateDepartment();
@@ -101,10 +102,10 @@ function accessEmployee() {
           })
           .then(function(answer) {
             if (answer.role === "ADD") {
-              addRole();
+                addRole();
             }
             else if(answer.role === "UPDATE") {
-                updateEmployee();
+                updateRole();
             } 
             else if(answer.role === "VIEW") {
                 viewRole();
@@ -117,113 +118,127 @@ function accessEmployee() {
             }
           });
         }
-  
-  // function to handle posting new items up for auction
-  function postAuction() {
-    // prompt for info about the item being put up for auction
-    inquirer
-      .prompt([
-        {
-          name: "item",
-          type: "input",
-          message: "What is the item you would like to submit?"
-        },
-        {
-          name: "category",
-          type: "input",
-          message: "What category would you like to place your auction in?"
-        },
-        {
-          name: "startingBid",
-          type: "input",
-          message: "What would you like your starting bid to be?",
-          validate: function(value) {
-            if (isNaN(value) === false) {
-              return true;
-            }
-            return false;
-          }
-        }
-      ])
+
+        function addEmployee() {
+            inquirer
+            .prompt ([
+                {
+                    name: "id",
+                    type: "number",
+                    message: "What the employee number?"
+                },
+                {
+                    name:"firstname",
+                    type: "input",
+                    message:"What is the employee's first name?"
+                },
+                {
+                    name:"lastname",
+                    type: "input",
+                    message:"What is the employee's last name?"
+                },
+                {
+                    name: "role-id",
+                    type: "number",
+                    message: "What is the employee role id number?"
+                },
+                {
+                    name: "manager-id",
+                    type: "number",
+                    message: "What the employee's manager id number?"
+                }
+            ])
       .then(function(answer) {
-        // when finished prompting, insert a new item into the db with that info
         connection.query(
-          "INSERT INTO auctions SET ?",
+          "INSERT INTO employee SET ?",
           {
-            item_name: answer.item,
-            category: answer.category,
-            starting_bid: answer.startingBid || 0,
-            highest_bid: answer.startingBid || 0
+            id: answer.id,
+            first_name: answer.firstname,
+            last_name: answer.lastname,
+            role_id: answer.role-id,
+            manager_id: answer.manager-id
           },
           function(err) {
             if (err) throw err;
-            console.log("Your auction was created successfully!");
-            // re-prompt the user for if they want to bid or post
+            console.log("Your employee was added successfully!");
             start();
           }
         );
       });
-  }
-  
-  function bidAuction() {
-    // query the database for all items being auctioned
-    connection.query("SELECT * FROM auctions", function(err, results) {
-      if (err) throw err;
-      // once you have the items, prompt the user for which they'd like to bid on
-      inquirer
-        .prompt([
-          {
-            name: "choice",
-            type: "rawlist",
-            choices: function() {
-              var choiceArray = [];
-              for (var i = 0; i < results.length; i++) {
-                choiceArray.push(results[i].item_name);
-              }
-              return choiceArray;
+      function updateEmployee() {
+            connection.query("SELECT * FROM employee", function(err, results) {
+              if (err) throw err;
+              inquirer
+                .prompt([
+                  {
+                    name: "choice",
+                    type: "rawlist",
+                    choices: function() {
+                      var choiceArray = [];
+                      for (var i = 0; i < results.length; i++) {
+                        choiceArray.push(results[i].lastname);
+                      }
+                      return choiceArray;
+                    },
+                    message: "What is the last name of the employee you'd like to update?"
+                  },
+                  {
+                    name: "bid",
+                    type: "input",
+                    message: "How much would you like to bid?"
+                  }
+                ])
+                .then(function(answer) {
+                  // get the information of the chosen item
+                  var chosenEmployee;
+                  for (var i = 0; i < results.length; i++) {
+                    if (results[i].lastname === answer.lastname) {
+                      chosenEmployee = results[i];
+                    }
+                  }
+        console.log("Updating employee file...\n");
+        var query = connection.query(
+          "UPDATE employee SET ? WHERE ?",
+          [
+            {
+              lastname: answer.lastname
             },
-            message: "What auction would you like to place a bid in?"
-          },
+          
+          ],
+          function(err, res) {
+            if (err) throw err;
+            console.log(res.affectedRows + " employee updated!\n");
+          }
+        );
+      
+        // logs the actual query being run
+        console.log(query.sql);
+      }
+      
+      function deleteEmployee() {
+        console.log("Deleting employee...\n");
+        connection.query(
+          "DELETE FROM employee WHERE ?",
           {
-            name: "bid",
-            type: "input",
-            message: "How much would you like to bid?"
+            lastname: answer.lastname
+          },
+          function(err, res) {
+            if (err) throw err;
+            console.log(res.affectedRows + " employee deleted!\n");
           }
-        ])
-        .then(function(answer) {
-          // get the information of the chosen item
-          var chosenItem;
-          for (var i = 0; i < results.length; i++) {
-            if (results[i].item_name === answer.choice) {
-              chosenItem = results[i];
-            }
-          }
-  
-          // determine if bid was high enough
-          if (chosenItem.highest_bid < parseInt(answer.bid)) {
-            // bid was high enough, so update db, let the user know, and start over
-            connection.query(
-              "UPDATE auctions SET ? WHERE ?",
-              [
-                {
-                  highest_bid: answer.bid
-                },
-                {
-                  id: chosenItem.id
-                }
-              ],
-              function(error) {
-                if (error) throw err;
-                console.log("Bid placed successfully!");
-                start();
-              }
-            );
-          }
-          else {
-            // bid wasn't high enough, so apologize and start over
-            console.log("Your bid was too low. Try again...");
-            start();
-          }
+        );
+      }
+      
+      function viewEmployee() {
+        console.log("Viewing all employees...\n");
+        connection.query("SELECT * FROM employee", function(err, res) {
+          if (err) throw err;
+          // Log all results of the SELECT statement
+          console.log(res);
+          connection.end();
         });
-    });
-  }
+  
+
+//-------------------------------------------------------------//
+        
+  
--- index.js
+++ index.js
@@ -15,19 +15,19 @@ connection.connect(function(err) {
   addEmployee();
 });
 
-function createProduct() {
-  console.log("Inserting a new product...\n");
-  var query = connection.query(
-    "INSERT INTO products SET ?",
-    {
-      flavor: "Rocky Road",
-      price: 3.0,
-      quantity: 50
-    },
-    function(err, res) {
-      if (err) throw err;
-      console.log(res.affectedRows + " employee added!\n");
-      // Call updateProduct AFTER the INSERT completes
-      updateRoster();
-    }
+// function createProduct() {
+  // console.log("Inserting a new product...\n");
+  // var query = connection.query(
+  //   "INSERT INTO products SET ?",
+  //   {
+  //     flavor: "Rocky Road",
+  //     price: 3.0,
+  //     quantity: 50
+    // },
+    // function(err, res) {
+    //   if (err) throw err;
+    //   console.log(res.affectedRows + " employee added!\n");
+    //   // Call updateProduct AFTER the INSERT completes
+    //   updateRoster();
+    // }
   );
